15b5a2cf0cb69e5f87b394794f99cca5467a12ff
dhruva011 <dhruva011@gmail.com>
2020-05-17 22:01:03 +0530

    Fifth Commit

--- src/org/sol/LeapYear.java
+++ src/org/sol/LeapYear.java
@@ -3,9 +3,11 @@ package org.sol;
 public class LeapYear {
 	private int year;
 	
-	//Fail for All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016) 
+	//All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016) 
 	public boolean isLeap() {
 		if(this.year%400==0)
+			return true;
+		if(this.year%4==0 && this.year%100!=0)
 			return true;
 		return false;
 	}
